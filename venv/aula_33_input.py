410a29655467ecd2e84dde803c3a8e5e9db5eb9e
elisomorovskimedeiros <eli.sm@hotmail.com>
2024-09-13 21:37:38 -0300

    commit aula34

---
+++ venv/aula_33_input.py
@@ -0,0 +1,8 @@
+num1 = input("Informe um número: ")
+num2 = input("Informe outro número: ")
+#realize a coerção dos dados despois de coletados,
+#para permitir fazer a verificação dos dados
+num1 = int(num1)
+num2 = int(num2)
+
+print(f"A soma dos dois números é {num1 + num2}")
---
+++ venv/aula_34_if_else_condicionais.py
@@ -0,0 +1,10 @@
+#exemplo de código com bloco de código condicional
+
+entrada = input('VocÇe quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print("Você entrou no sistema!")
+elif entrada ==  'sair':
+    print("Você saiu do sistema!")
+else:
+    print("Você não digitou nem entrar e nem sair!")
